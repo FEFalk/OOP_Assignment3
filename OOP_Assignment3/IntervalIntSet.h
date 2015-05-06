d4e20e43254e698fa475760849367086ceccc96c
FEFalk <fille_945@hotmail.com>
2015-05-06 16:29:20 +0200

    La till de olika klasserna och en main

---
+++ OOP_Assignment3/IntervalIntSet.h
@@ -0,0 +1,8 @@
+#pragma once
+class IntervalIntSet
+{
+public:
+	IntervalIntSet();
+	~IntervalIntSet();
+};
+
---
+++ OOP_Assignment3/UnionIntSet.h
@@ -0,0 +1,8 @@
+#pragma once
+class UnionIntSet
+{
+public:
+	UnionIntSet();
+	~UnionIntSet();
+};
+
